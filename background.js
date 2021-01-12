3fc5251ad1339fef6e79714c4e7f6be7572a2c3e
Reid Metzger <contact@reidmetzger.com>
2021-01-11 19:33:11 -0500

    modify background.js
    search and close works now

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-var allowedSite = "https://www.google.com/"
+var allowedSite = "google.com"
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.action == "testSite") {
-            if(sender.tab.url !== allowedSite){
-                alert(sender.tab.url)
+            if(-1 == sender.tab.url.search(allowedSite)){
+                //alert(sender.tab.url)
                 chrome.tabs.remove(sender.tab.id);
             }
         }
