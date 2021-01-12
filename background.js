1bc2b372844989f3478bcd1113e9c0bff86c3b30
Reid Metzger <contact@reidmetzger.com>
2021-01-11 19:17:33 -0500

    delete any opened tab
    for testing

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.action == "testSite") {
             alert(sender.tab.url)
+            chrome.tabs.remove(sender.tab.id);
         }
     }
 );
