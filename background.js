3d151dc51296d6c316b16a764df84009cb8f6b91
Reid Metzger <contact@reidmetzger.com>
2021-01-11 20:07:49 -0500

    add local variable storage

--- background.js
+++ background.js
@@ -1,12 +1,24 @@
-var allowedSite = "google.com"
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("This is a first install!");
+        chrome.storage.local.set({"active": false});
+        chrome.storage.local.set({"lockedSite": "google.com"});
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+    }
+});
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.action == "testSite") {
-            if(-1 == sender.tab.url.search(allowedSite)){
-                //alert(sender.tab.url)
-                chrome.tabs.remove(sender.tab.id);
-            }
+            chrome.storage.local.get(['lockedSite'], function (result) {
+                if (-1 == sender.tab.url.search(result.key)) {
+                    //alert(sender.tab.url)
+                    chrome.tabs.remove(sender.tab.id);
+                }
+            });
         }
     }
 );
