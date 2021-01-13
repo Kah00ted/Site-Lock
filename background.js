c2bf77b028e25d3565cbd2b373b5b197f4d4d745
Reid Metzger <contact@reidmetzger.com>
2021-01-12 19:59:48 -0500

    Config updates

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // Check whether new version is installed
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason == "install") {  //run on extension installed
-        console.log("This is a first install!");
-        chrome.storage.local.set({ "active": true });
-        chrome.storage.local.set({ "lockedSite": "example.com" });
+        //console.log("This is a first install!");
+        chrome.storage.local.set({ "active": false });
+        chrome.storage.local.set({ "lockedSite": "" });
     } else if (details.reason == "update") {  //run on extension updated
         var thisVersion = chrome.runtime.getManifest().version;
         console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
